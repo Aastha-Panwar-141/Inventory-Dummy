35af0435d73871273f1c6dc1edc7c4faadec55f9
Aastha-Panwar-141 <asthap@shriffle.com>
2023-08-28 16:11:23 +0530

    before adding a enumeration

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,20 @@
 class Product < ApplicationRecord
   validates :name, :brand_name, presence: true
-  belongs_to :vendor
-  has_many :product_variants
+  belongs_to :vendor, optional: true
+  has_many :product_variants, dependent: :destroy
+
+  after_create :product_created
+  after_commit :callback_after_commit
+
+  scope :product_length, -> {where(product_variants: length)}
+
+  def product_created
+    puts "Product Created Successfully!"
+  end
+
+  def callback_after_commit
+    puts "Message after commit!"
+  end
+
+
 end
---
+++ db/migrate/20230825131507_change_column_in_products.rb
@@ -0,0 +1,5 @@
+class ChangeColumnInProducts < ActiveRecord::Migration[7.0]
+  def change
+    # change_column_null :products, :vendor_id, true
+  end
+end
